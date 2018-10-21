601d79c6e6cce5a2ba07406fb7442d8297324828
joewstroman <joewstroman@gmail.com>
2018-10-20 20:33:31 -0400

    Rest Api working for hosts outside of its container

--- restapi/wait-for-db.sh
+++ restapi/wait-for-db.sh
@@ -1,19 +1,31 @@
-#!/usr/bin/env bash
+# #!/usr/bin/env bash
+
+# set -e
+
+# host="$1"
+# shift
+# cmd="$@"
+
+# echo [mysql] > ~/.my.cnf
+# echo user = root >> ~/.my.cnf
+# echo password = $MYSQL_PASSWORD >> ~/.my.cnf
+
+# until ping -c1 db &>/dev/null; do
+#   >&2 echo "MYSQL is unavailable - sleeping"
+#   sleep 10
+# done
+
+# >&2 echo "MYSQL is up - executing command"
+# exec $cmd
 
 set -e
 
-host="$1"
-shift
-cmd="$@"
-
-echo [mysql] > ~/.my.cnf
-echo user = root >> ~/.my.cnf
-echo password = $MYSQL_ROOT_PASSWORD >> ~/.my.cnf
-
-until curl $host:3306; do
-  >&2 echo "MYSQL is unavailable - sleeping"
-  sleep 10
+until nc -z -v -w30 $DB_HOST 3306
+do
+  echo "Waiting for database connection..."
+  # wait for 5 seconds before check again
+  sleep 5
 done
 
->&2 echo "MYSQL is up - executing command"
-exec $cmd
+echo "Mysql is up - executing command"
+xmysql -h $DB_HOST -p $DB_PASSWORD -d $DB_NAME -u $DB_USER -n 80 -r 0.0.0.0
